77fc5a5eb391011cc57976c4a61b75cc55b03da7
meet patel <meet56256@gmail.com>
2023-01-11 18:57:36 +0530

    first

--- lib/T.dart
+++ lib/T.dart
@@ -1,6 +1,3 @@
-import 'dart:core';
-import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +13,7 @@ final firebase = FirebaseFirestore.instance;
 final currentuser = FirebaseAuth.instance;
 
 class _TState extends State<T> {
-  late List dropDownListData;
+  // late List dropDownListData;
   // {
   //   'title':
   //       FirebaseFirestore.instance.collection("Projects").doc().toString(),
@@ -28,66 +25,63 @@ class _TState extends State<T> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    dropDownListData = [firebase.collection("Projects").doc().toString()];
+    // dropDownListData = [firebase.collection("Projects").doc().toString()];
   }
 
   @override
   Widget build(BuildContext context) {
-    print("data ${firebase.collection("Projects")}");
+    // print("data ${firebase.collection("Projects")}");
     return Scaffold(
-        // body: Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   crossAxisAlignment: CrossAxisAlignment.center,
-        //   children: [
-        //     SizedBox(
-        //       height: 50,
-        //       child: ListView(
-        //         children: [
-        //           DropdownButton<String>(
-        //               isDense: true,
-        //               value: dropDownValue,
-        //               isExpanded: true,
-        //               menuMaxHeight: 350,
-        //               items: [
-        //                 DropdownMenuItem(
-        //                   child: Text("Select course"),
-        //                   value: "Select course",
-        //                 ),
-        //                 ...dropDownListData.map<DropdownMenuItem<String>>((e) {
-        //                   return DropdownMenuItem(
-        //                     child: StreamBuilder(
-        //                       stream: firebase
-        //                           .collection('Projects')
-        //                           .doc()
-        //                           .snapshots(),
-        //                       builder: (BuildContext context,
-        //                           AsyncSnapshot<dynamic> snapshot) {
-        //                         if (snapshot.hasData) {
-        //                           return ListView.builder(
-        //                               itemCount: snapshot.data!.docs.length,
-        //                               itemBuilder: (context, i) {
-        //                                 var data = snapshot.data!.docs[i];
-        //                                 return Text(data['name']);
-        //                               });
-        //                         }
-        //                         return const CircularProgressIndicator();
-        //                       },
-        //                     ),
-        //                     value: e['value'],
-        //                   );
-        //                 })
-        //               ],
-        //               onChanged: (newvalue) {
-        //                 log("selected value ====${newvalue}");
-        //                 setState(() {
-        //                   dropDownValue = newvalue!;
-        //                 });
-        //               })
-        //         ],
-        //       ),
-        //     )
-        //   ],
-        // ),
-        );
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 300,
+          ),
+          Expanded(
+            child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("Projects")
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  return ListView(children: [
+                    SizedBox(
+                      height: 50,
+                      child: DropdownButton(
+                          isDense: false,
+                          value: dropDownValue,
+                          isExpanded: true,
+                          menuMaxHeight: 350,
+                          items: [
+                            DropdownMenuItem(
+                              value: snapshot.data!.docs.toString(),
+                              child: ListView.builder(
+                                  itemCount: snapshot.data!.docs.length,
+                                  shrinkWrap: true,
+                                  itemBuilder: (context, i) {
+                                    var data = snapshot.data!.docs[i];
+                                    return Text(data['name']);
+                                  }),
+                            ),
+                          ],
+                          onChanged: (newvalue) {
+                            setState(() {
+                              print('a');
+                              dropDownValue = newvalue!;
+                            });
+                          }),
+                    ),
+                  ]);
+                  // return CircularProgressIndicator();
+                  if (snapshot.hasData) {
+                  } else {
+                    const CircularProgressIndicator();
+                  }
+                }),
+          )
+        ],
+      ),
+    );
   }
 }
